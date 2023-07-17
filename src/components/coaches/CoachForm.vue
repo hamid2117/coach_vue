3944618da8a8f123b4ddfe3800de5735b20e2821
hamid2117 <saim008865@gmail.com>
2023-07-17 10:24:57 +0500

    created form for register coach

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -1,37 +1,37 @@
 <template>
-  <form @submit.prevent="submitForm">
+  <form @submit.prevent="handleSubmit">
     <div class="form-control">
       <label for="firstname">Firstname</label>
-      <input type="text" id="firstname" />
+      <input type="text" id="firstname" v-model.trim="firstName" />
       <p v-if="!firstName.isValid">Firstname must not be empty.</p>
     </div>
     <div class="form-control">
       <label for="lastname">Lastname</label>
-      <input type="text" id="lastname" />
+      <input type="text" id="lastname" v-model.trim="lastName" />
       <p v-if="!lastName.isValid">Lastname must not be empty.</p>
     </div>
     <div class="form-control">
       <label for="description">Description</label>
-      <textarea id="description" rows="5"></textarea>
+      <textarea id="description" rows="5" v-model.trim="description"></textarea>
       <p>Description must not be empty.</p>
     </div>
     <div class="form-control">
       <label for="rate">Hourly Rate</label>
-      <input type="number" id="rate" />
+      <input type="number" id="rate" v-model.number="rate" />
       <p>Rate must be greater than 0.</p>
     </div>
     <div class="form-control">
       <h3>Areas of Expertise</h3>
       <div>
-        <input type="checkbox" id="frontend" value="frontend" />
+        <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
         <label for="frontend">Frontend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="backend" value="backend" />
+        <input type="checkbox" id="backend" value="backend" v-model="areas" />
         <label for="backend">Backend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="career" value="career" />
+        <input type="checkbox" id="career" value="career" v-model="areas" />
         <label for="career">Career Advisory</label>
       </div>
       <p>At least one expertise must be selected.</p>
@@ -42,7 +42,29 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      description: '',
+      rate: null,
+      areas: []
+    }
+  },
+  methods: {
+    handleSubmit() {
+      const formData = {
+        first: this.firstName,
+        last: this.lastName,
+        desc: this.description,
+        rate: this.rate,
+        areas: this.areas
+      }
+      console.log(formData)``
+    }
+  }
+}
 </script>
 
 <style scoped>
--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,7 +1,11 @@
+<script setup>
+import CoachForm from '../../components/coaches/CoachForm.vue'
+</script>
 <template>
   <section>
     <base-card>
       <h2>Register as a coach now!</h2>
+      <coach-form />
     </base-card>
   </section>
 </template>
